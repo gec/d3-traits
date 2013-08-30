ad911e7edf59d248bf923c6fd63ff0964ad1f3a5
Flint O'Brien <fobrien@greenenergycorp.com>
2013-08-30 11:00:47 -0400

    initial checkin.

---
+++ Gruntfile.js
@@ -0,0 +1,97 @@
+/*global module:false, require:false*/
+module.exports = function(grunt) {
+  'use strict';
+
+  // Load Deps
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  // Travis doesn't have chrome, so we need to overwrite some options
+  var testConfig = function(configFile, customOptions) {
+    var options = { configFile: configFile, keepalive: true };
+    var travisOptions = process.env.TRAVIS && { browsers: ['Firefox'], reporters: 'dots' };
+    return grunt.util._.extend(options, customOptions, travisOptions);
+  };
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> d3-traits;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+
+    watch: {
+      files: ['lib/<%= pkg.name %>.js', 'test/*.spec.js'],
+      tasks: ['jshint', 'concat', 'karma:continuous', 'uglify']
+    },
+
+    karma: {
+      options: testConfig('karma.conf.js'),
+
+      continuous: {
+        singleRun: true,
+        autoWatch: false,
+        browsers: ['PhantomJS']
+      },
+
+      unit: {
+        singleRun: true,
+        autoWatch: false,
+        coverageReporter: {
+          type : 'html',
+          dir : 'coverage/'
+        },
+        browsers: ['PhantomJS']
+      }
+    },
+
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      js: {
+        src: ['lib/<%= pkg.name %>.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      },
+      css: {
+        src: ['lib/<%= pkg.name %>.css'],
+        dest: 'dist/<%= pkg.name %>.css'
+      },
+      test: {
+        src: ['test/spec.prefix', 'test/*.spec.js' ,'test/spec.suffix'],
+        dest: 'dist/<%= pkg.name %>.spec.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      js: {
+        src: '<%= concat.js.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      test: {
+        src: ['lib/**/*.js']
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'karma:continuous']);
+
+  grunt.registerTask('fast-build', ['concat', 'uglify']);
+  grunt.registerTask('coverage', ['concat', 'karma:unit']);
+};
