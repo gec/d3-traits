35c6f3d0fc3abe1bdf9c915a68acb953d6b02353
Flint O'Brien <flint@hungrylearner.com>
2014-08-09 01:29:04 -0400

    Added comment for Array.isArray usage.

--- src/d3-traits.js
+++ src/d3-traits.js
@@ -40,6 +40,7 @@
 
   var DEBUG = false
 
+  // Usage: Array.isArray( myArray)
   Array.isArray = Array.isArray || function(vArg) {
     return Object.prototype.toString.call(vArg) === "[object Array]";
   };
